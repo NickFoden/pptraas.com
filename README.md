4f30a20dc533f324b321e41ce5833f607de3dd37
Nick Foden <nickfoden@gmail.com>
2020-04-20 13:28:15 -0400

    First modification pass

--- README.md
+++ README.md
@@ -1,47 +1,54 @@
-<a href="https://github.com/GoogleChromeLabs/pptraas.com"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+## Modfied
 
-Puppeteer as a service
-======================
+This is a modified fork of Puppeteer as a service to deploy a copy since the original is now no longer up / maintainted.
+
+# puptraas
+
+<a href="https://github.com/NickFoden/pptraas.com"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 
 ## Render
 
 ### Render page as a PNG
-https://pptraas.com/screenshot?url=https://paul.kinlan.me/ (full page)
 
-https://pptraas.com/screenshot?url=https://developers.google.com&size=400,400
+https://puptraas.herokuapp.com/screenshot?url=https://www.liberiachimpanzeerescue.org/ (full page)
 
-https://pptraas.com/screenshot?url=https://www.wikipedia.org&element=.central-featured
+https://puptraas.herokuapp.com/screenshot?url=https://developers.google.com&size=400,400
+
+https://puptraas.herokuapp.com/screenshot?url=https://www.wikipedia.org&element=.central-featured
 
 ### Render page as a PDF
-https://pptraas.com/pdf?url=https://paul.kinlan.me/
+
+https://puptraas.herokuapp.com/pdf?url=https://www.liberiachimpanzeerescue.org/
 
 ### Render generated static markup of page ("SSR")
-https://pptraas.com/ssr?url=https://angular2-hn.firebaseapp.com/
+
+https://puptraas.herokuapp.com/ssr?url=https://angular2-hn.firebaseapp.com/
 
 ### Render as Google Search bot
 
 Detects what features a page is using and cross references them with the features
 supported by the [Google Search bot](https://developers.google.com/search/docs/guides/rendering).
 
-https://pptraas.com/gsearch?url=https://paul.kinlan.me/
+https://puptraas.herokuapp.com/gsearch?url=https://www.liberiachimpanzeerescue.org/
 
 ## Performance
 
 ### Get a timeline trace
 
-https://pptraas.com/trace?url=https://paul.kinlan.me/
+https://puptraas.herokuapp.com/trace?url=https://www.liberiachimpanzeerescue.org/
 
 #### View the trace in trace-viewer:
 
-https://chromedevtools.github.io/timeline-viewer/?loadTimelineFromURL=https://pptraas.com/trace?url=https://paul.kinlan.me/
+https://chromedevtools.github.io/timeline-viewer/?loadTimelineFromURL=https://puptraas.herokuapp.com/trace?url=https://www.liberiachimpanzeerescue.org/
 
 ### Get metrics
-https://pptraas.com/metrics?url=https://paul.kinlan.me/
+
+https://puptraas.herokuapp.com/metrics?url=https://www.liberiachimpanzeerescue.org/
 
 ## Misc
 
 ### Print UserAgent / Chromium version
-https://pptraas.com/version
 
-----
-[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/GoogleChromeLabs/pptraas.com) [![Lighthouse score: 100/100](https://lighthouse-badge.appspot.com/?score=100&category=Perf)](https://github.com/ebidel/lighthouse-badge)
+https://puptraas.herokuapp.com/version
+
+---
--- server.js
+++ server.js
@@ -14,57 +14,50 @@
  * limitations under the License.
  */
 
-const express = require('express');
-const puppeteer = require('puppeteer');
-const randomUUID = require('random-uuid');
-const fs = require('fs');
-const util = require('util');
-const marked = require('marked');
-const ua = require('universal-analytics');
-const {URL} = require('url');
-const gsearch = require('./helpers/gsearch.js');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const randomUUID = require("random-uuid");
+const fs = require("fs");
+const util = require("util");
+const marked = require("marked");
+const { URL } = require("url");
+const gsearch = require("./helpers/gsearch.js");
 
 const PORT = process.env.PORT || 8080;
-const GA_ACCOUNT = 'UA-114816386-1';
 const app = express();
 
 const isAllowedUrl = (string) => {
   try {
     const url = new URL(string);
-    return url.hostname !== 'pptraas.com' && !url.hostname.startsWith('puppeteerexamples');
+    return url.hostname !== "puptraas.herokuapp.com/";
   } catch (err) {
     return false;
   }
 };
-// Adds cors, records analytics hit, and prevents self-calling loops.
+
 app.use((request, response, next) => {
   const url = request.query.url;
   if (url && !isAllowedUrl(url)) {
     return response.status(500).send({
-      error: 'URL is either invalid or not allowed'
+      error: "URL is either invalid or not allowed",
     });
   }
 
-  response.set('Access-Control-Allow-Origin', '*');
-
-  // Record GA hit.
-  const visitor = ua(GA_ACCOUNT, {https: true});
-  visitor.pageview(request.originalUrl).send();
+  response.set("Access-Control-Allow-Origin", "*");
 
   next();
 });
 
-app.get('/', async (request, response) => {
+app.get("/", async (request, response) => {
   const readFile = util.promisify(fs.readFile);
-  const md = (await readFile('./README.md', {encoding: 'utf-8'}));
+  const md = await readFile("./README.md", { encoding: "utf-8" });
   /* eslint-disable */
   response.send(`
     <!DOCTYPE html>
     <html>
     <head>
-      <title>Puppeteer as a service</title>
+      <title>Puppeteer as a service (Modified)</title>
       <meta name="description" content="A hosted service that makes the Chrome Puppeteer API accessible via REST based queries. Tracing, Screenshots and PDFs" />
-      <meta name="google-site-verification" content="4Tf-yH47m_tR7aSXu7t3EI91Gy4apbwnhg60Jzq_ieY" />
       <style>
         body {
           padding: 40px;
@@ -76,53 +69,47 @@ app.get('/', async (request, response) => {
       </style>
     </head>
     <body>${marked(md)}</body>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=${GA_ACCOUNT}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
 
-      gtag('config', '${GA_ACCOUNT}');
-    </script>
     </html>
   `);
   /* eslint-enable */
 });
 
 // Init code that gets run before all request handlers.
-app.all('*', async (request, response, next) => {
+app.all("*", async (request, response, next) => {
   response.locals.browser = await puppeteer.launch({
     dumpio: true,
     // headless: false,
     // executablePath: 'google-chrome',
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], // , '--disable-dev-shm-usage']
+    args: ["--no-sandbox", "--disable-setuid-sandbox"], // , '--disable-dev-shm-usage']
   });
 
   next(); // pass control on to routes.
 });
 
-app.get('/screenshot', async (request, response) => {
+app.get("/screenshot", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   // Default to a reasonably large viewport for full page screenshots.
   const viewport = {
     width: 1280,
     height: 1024,
-    deviceScaleFactor: 2
+    deviceScaleFactor: 2,
   };
 
   let fullPage = true;
   const size = request.query.size;
   if (size) {
-    const [width, height] = size.split(',').map(item => Number(item));
+    const [width, height] = size.split(",").map((item) => Number(item));
     if (!(isFinite(width) && isFinite(height))) {
-      return response.status(400).send(
-        'Malformed size parameter. Example: ?size=800,600');
+      return response
+        .status(400)
+        .send("Malformed size parameter. Example: ?size=800,600");
     }
     viewport.width = width;
     viewport.height = height;
@@ -135,7 +122,7 @@ app.get('/screenshot', async (request, response) => {
   try {
     const page = await browser.newPage();
     await page.setViewport(viewport);
-    await page.goto(url, {waitUntil: 'networkidle0'});
+    await page.goto(url, { waitUntil: "networkidle0" });
 
     const opts = {
       fullPage,
@@ -147,7 +134,7 @@ app.get('/screenshot', async (request, response) => {
         x: 0,
         y: 0,
         width: viewport.width,
-        height: viewport.height
+        height: viewport.height,
       };
     }
 
@@ -157,14 +144,13 @@ app.get('/screenshot', async (request, response) => {
     if (element) {
       const elementHandle = await page.$(element);
       if (!elementHandle) {
-        return response.status(404).send(
-          `Element ${element} not found`);
+        return response.status(404).send(`Element ${element} not found`);
       }
       buffer = await elementHandle.screenshot();
     } else {
       buffer = await page.screenshot(opts);
     }
-    response.type('image/png').send(buffer);
+    response.type("image/png").send(buffer);
   } catch (err) {
     response.status(500).send(err.toString());
   }
@@ -172,56 +158,59 @@ app.get('/screenshot', async (request, response) => {
   await browser.close();
 });
 
-app.get('/metrics', async (request, response) => {
+app.get("/metrics", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   const browser = response.locals.browser;
   const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle0'});
+  await page.goto(url, { waitUntil: "networkidle0" });
   const metrics = await page.metrics();
   await browser.close();
 
-  response.type('application/json').send(JSON.stringify(metrics));
+  response.type("application/json").send(JSON.stringify(metrics));
 });
 
-app.get('/pdf', async (request, response) => {
+app.get("/pdf", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   const browser = response.locals.browser;
 
   const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle0'});
+  await page.goto(url, { waitUntil: "networkidle0" });
   const pdf = await page.pdf();
   await browser.close();
 
-  response.type('application/pdf').send(pdf);
+  response.type("application/pdf").send(pdf);
 });
 
-app.get('/ssr', async (request, response) => {
+app.get("/ssr", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   const browser = response.locals.browser;
 
   try {
     const page = await browser.newPage();
-    const res = await page.goto(url, {waitUntil: 'networkidle0'});
+    const res = await page.goto(url, { waitUntil: "networkidle0" });
 
     // Inject <base> on page to relative resources load properly.
-    await page.evaluate(url => {
+    await page.evaluate((url) => {
       /* global document */
-      const base = document.createElement('base');
+      const base = document.createElement("base");
       base.href = url;
       document.head.prepend(base); // Add to top of head, before all other resources.
     }, url);
@@ -229,8 +218,9 @@ app.get('/ssr', async (request, response) => {
     // Remove scripts(except structured data) and html imports. They've already executed and loaded on the page.
     await page.evaluate(() => {
       const elements = document.querySelectorAll(
-        'script:not([type="application/ld+json"]), link[rel="import"]');
-      elements.forEach(e => e.remove());
+        'script:not([type="application/ld+json"]), link[rel="import"]'
+      );
+      elements.forEach((e) => e.remove());
     });
 
     const html = await page.content();
@@ -242,11 +232,12 @@ app.get('/ssr', async (request, response) => {
   await browser.close();
 });
 
-app.get('/trace', async (request, response) => {
+app.get("/trace", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   const browser = response.locals.browser;
@@ -254,13 +245,13 @@ app.get('/trace', async (request, response) => {
 
   const page = await browser.newPage();
   try {
-    page.on('error', error => {
+    page.on("error", (error) => {
       console.log(url, error);
     });
-    await page.tracing.start({path: filename, screenshots: true});
-    await page.goto(url, {waitUntil: 'networkidle0'});
+    await page.tracing.start({ path: filename, screenshots: true });
+    await page.goto(url, { waitUntil: "networkidle0" });
     await page.tracing.stop();
-    response.type('application/json').sendFile(filename);
+    response.type("application/json").sendFile(filename);
   } catch (e) {
     response.status(500).send(e.toString());
   }
@@ -268,22 +259,27 @@ app.get('/trace', async (request, response) => {
   await browser.close();
 });
 
-app.get('/version', async (request, response) => {
+app.get("/version", async (request, response) => {
   const browser = response.locals.browser;
   const ua = await browser.userAgent();
   await browser.close();
   response.send(ua);
 });
 
-app.get('/gsearch', async (request, response) => {
+app.get("/gsearch", async (request, response) => {
   const url = request.query.url;
   if (!url) {
-    return response.status(400).send(
-      'Please provide a URL. Example: ?url=https://example.com');
+    return response
+      .status(400)
+      .send("Please provide a URL. Example: ?url=https://example.com");
   }
 
   const browser = response.locals.browser;
-  const results = await gsearch.run(browser, url, `/tmp/trace-${randomUUID()}.json`);
+  const results = await gsearch.run(
+    browser,
+    url,
+    `/tmp/trace-${randomUUID()}.json`
+  );
   await browser.close();
 
   const style = `
@@ -310,21 +306,32 @@ app.get('/gsearch', async (request, response) => {
   response.send(style + results);
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`App is listening on port ${PORT}`);
 });
 
 // Make sure node server process stops if we get a terminating signal.
 function processTerminator(sig) {
-  if (typeof sig === 'string') {
+  if (typeof sig === "string") {
     process.exit(1);
   }
-  console.log('%s: Node server stopped.', Date(Date.now()));
+  console.log("%s: Node server stopped.", Date(Date.now()));
 }
 
 const signals = [
-  'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
-  'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'];
-signals.forEach(sig => {
+  "SIGHUP",
+  "SIGINT",
+  "SIGQUIT",
+  "SIGILL",
+  "SIGTRAP",
+  "SIGABRT",
+  "SIGBUS",
+  "SIGFPE",
+  "SIGUSR1",
+  "SIGSEGV",
+  "SIGUSR2",
+  "SIGTERM",
+];
+signals.forEach((sig) => {
   process.once(sig, () => processTerminator(sig));
 });
